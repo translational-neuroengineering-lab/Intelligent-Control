2481e0fefb7087a37b43066008cc71801b64eb9b
TDT <TDT@170.140.232.237>
2016-02-02 18:28:24 -0500

    Updated to include a scrolling plot and spectrogram using circBuf

--- In Vivo/bus/matlab/closed_loop_template.m
+++ In Vivo/bus/matlab/closed_loop_template.m
@@ -1,17 +1,29 @@
-close all; clear all; clc;
+close all;  clc;
+
+% Set up connection
+if ~exist('TD', 'var')
+    TD = TDEV();
+end
+TD.preview
+TD_FS   = TD.FS{1};
 
 % Configure Settings
-MAX_READ_BUFFER_SIZE    = 16*5000;
-MAX_WRITE_BUFFER_SIZE   = 16*50000;
+BUFFER_TIME             = .5;
+
+TD_READ_BUFFER_SIZE     = 16*ceil(TD_FS*BUFFER_TIME);
+TD_WRITE_BUFFER_SIZE    = 16*ceil(TD_FS*BUFFER_TIME);
 OPEN_VALUE              = 10000;
 
 STIM_CHAN               = [1 3 5 7 10 12 14 16];
 READ_CHAN               = [2 4 6 8 9  11 13 15];
 
-% Set up connection
-TD = TDEV();
-TD.preview
-TD.write('read_durr', MAX_READ_BUFFER_SIZE);
+DATA_VEC_SIZE           = 16;
+DATA_BUFFER_SIZE        = ceil(TD_FS*BUFFER_TIME)*10;
+
+DATA_BUFFER             = circVBuf(int64(DATA_BUFFER_SIZE), int64(DATA_VEC_SIZE), 0);
+FREQ_BUFFER             = circVBuf(int64(10), int64(ceil(TD_FS*BUFFER_TIME/4)), 0);
+
+TD.write('read_durr', TD_READ_BUFFER_SIZE);
 
 % Open all stimulation channels
 for c1 = STIM_CHAN
@@ -20,9 +32,9 @@ for c1 = STIM_CHAN
 end
 
 % Get acquisition circuit variables
-n_read_pts  = length(TD.read('read_buff'));
+n_read_pts  = TD_READ_BUFFER_SIZE;%length(TD.read('read_buff'));
 n_buff_pts  = n_read_pts/2; 
-t           = (1:n_buff_pts)/TD.FS{1};
+t           = (1:n_buff_pts)/TD_FS;
 
 read_index  = TD.read('read_index');
 curindex    = TD.read('read_index');
@@ -35,23 +47,34 @@ while true
     % Read data from the first half of the buffer
     while(curindex < n_buff_pts)
         curindex = TD.read('read_index');
-        fprintf('Current %d, Waiting for %d\n',curindex, length(TD.read('read_buff')));
         pause(.05);
     end
   
-    % Reshape and copy data to end of internal buffer
-    internal_buff(n_buff_pts + 1:n_buff_pts * 2) = ...
-        reshape(TD.read('read_buff', 'SIZE', n_buff_pts), 16, n_buff_pts/16);
-        
+    % Reshape and copy data to end of internal buffer   
+    new_data = reshape(TD.read('read_buff', 'SIZE', n_buff_pts), 16, n_buff_pts/16)';
+    DATA_BUFFER.append(new_data);
+    
     % Process Data 
-    signal_power = sum(internal_buff(1,:).^2)/size(internal_buff(1,:),2)*1e6;
+    signal_power = sum(new_data(:,2).^2)/size(new_data(:,2),1)*1e6;
+    f = fft(new_data(:,2));
+    FREQ_BUFFER.append(abs(f(1:end/2))');
     
     % Generate Signal
+    subplot(2,1,1)
+    t = double((1:DATA_BUFFER.lst-DATA_BUFFER.fst+1))/TD_FS;
+    plot(t, DATA_BUFFER.raw(DATA_BUFFER.fst:DATA_BUFFER.lst,2));
+    xlim([0, t(end)]);
+    subplot(2,1,2)
+    spectrogram(DATA_BUFFER.raw(DATA_BUFFER.fst:DATA_BUFFER.lst,2),128,120,128,TD.FS{1},'yaxis')
+    ylim([0 1])
+    colorbar off
     output = sprintf('Power = %.4f\n', signal_power);
+
     % arr = [zeros(1,1) ones(1,100) -ones(1,100)];
 
     % Send Signal
     disp(output);
+    
     % TD.write('stim_buff~4', arr);
     % TD.write('stim_dur~4', numel(arr));
     
@@ -62,8 +85,9 @@ while true
     end
     
     % Copy data to begining of internal buffer
-    internal_buff(1:n_buff_pts) = ...
-        reshape(TD.read('read_buff', 'OFFSET', n_buff_pts, 'SIZE', n_buff_pts), 16, n_buff_pts/16);
+%     internal_buff(1:n_buff_pts) = ...
+%         reshape(TD.read('read_buff', 'OFFSET', n_buff_pts, 'SIZE', n_buff_pts), 16, n_buff_pts/16);
+    DATA_BUFFER.append(reshape(TD.read('read_buff', 'OFFSET', n_buff_pts, 'SIZE', n_buff_pts), 16, n_buff_pts/16)');
     
     % Check read speed
     curindex = TD.read('read_index');
