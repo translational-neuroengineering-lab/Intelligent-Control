5853f4aaf378273da2b3c9ac851598673d9d4a17
Mark Connolly <mark.connolly@emory.edu>
2016-04-15 14:02:54 -0400

    "Initial check-in of asynchronous stimulation"

---
+++ In Vivo/bus/matlab/signal_generation/generate_asynchronous.m
@@ -0,0 +1,24 @@
+function stim_matrix = generate_asynchronous(sampling_frequency,     ...
+                                stimulation_frequency,  ...
+                                duration,               ...
+                                amplitude,              ...
+                                pulse_width,            ...
+                                n_channels)
+
+stim_order  = randperm(n_channels);
+s           = generate_biphasic(sampling_frequency, stimulation_frequency/n_channels, duration*n_channels, amplitude, pulse_width);
+stim_matrix = zeros(n_channels,size(s,2));
+
+for c1 = 1:n_channels
+    
+    start_sample                    = floor((c1-1)*sampling_frequency/stimulation_frequency + 1);
+    offset_stim                     = [zeros(1,start_sample-1) s];
+    stim_matrix(stim_order(c1),:)   = offset_stim(1:size(stim_matrix,2));
+    
+end
+
+
+stim_matrix = stim_matrix(:,1:sampling_frequency*duration);
+
+end
+
